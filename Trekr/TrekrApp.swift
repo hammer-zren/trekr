ce4f75e04d36671e96f3f89f4bb3b2d28e039f30
hammer-zren <hammer.zren@gmail.com>
2021-10-19 15:00:43 +1100

    swiftUI demo project

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by ZHONGTAO REN on 19/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
